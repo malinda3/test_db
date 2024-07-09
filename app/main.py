65ec2f75f5c3622decf95d1d07f4ce0796289127
malindac <malindac@student.21-school.ru>
2024-07-09 10:55:04 +0300

    test

--- app/main.py
+++ app/main.py
@@ -1,19 +1,10 @@
 from fastapi import FastAPI, Depends
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship, Session
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.declarative import DeclarativeMeta
-
-from .db import Base, get_db, User, Gender, City, Preference, Goal
-
-DATABASE_URL = "postgresql://admin:password@db/dating_db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
+from sqlalchemy.orm import Session
+from .db import SessionLocal, Base, get_db, User
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=SessionLocal().get_bind())
 
 @app.get("/users/")
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
